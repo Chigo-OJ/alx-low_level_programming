42052995ba5f11447774312108683332afeb5ce7
Chigo-OJ <chigozirimosuebi@gmail.com>
2023-03-16 17:22:21 -0700

    my first c code

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,13 @@
+#include <studio.h>
+
+/**
+ * main - This function begins my coding
+ *
+ *Return: this ends my code block
+ */
+
+int main(void)
+{
+	puts("\"programming is like building a multilingual puzzle);
+	return (0);
+}
