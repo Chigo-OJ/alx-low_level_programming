4b30ad81d76bd79908376778f3b5f2ebf4706c63
Chigo-OJ <chigozirimosuebi@gmail.com>
2023-03-17 14:48:22 -0700

    all the alphabet

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - code starts here
+ *
+ * Return: code ends here
+ */
+int main(void)
+{
+	char i;
+
+	for (i = 'a'; i <= 'z'; i++)
+		putchar(i);
+	putchar('\n');
+	return (0);
+}
