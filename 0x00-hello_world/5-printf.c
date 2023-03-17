40ecba2918cf7b9bb03bd9e774ef578ac207712b
Chigo-OJ <chigozirimosuebi@gmail.com>
2023-03-16 18:02:59 -0700

    printf c coding

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - starts my code block
+ *
+ * Return: ends my code block
+ */
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,");
+	return (0);
+}
