8aa0c6bbfeb4fe8ea66c762ffe4c7222d6c41701
Chigo-OJ <chigozirimosuebi@gmail.com>
2023-03-17 18:38:11 -0700

    numbers

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main -start of code
+ *
+ * Return: end of code
+ */
+
+int main(void)
+{
+	int i;
+
+	for (i = 0; i < 10; i++)
+	{
+		putchar(i + '0');
+		putchar(',');
+		putchar(' ');
+	}
+	return (0);
+}
