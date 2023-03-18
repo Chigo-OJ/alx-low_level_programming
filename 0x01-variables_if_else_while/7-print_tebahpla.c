72a31d2f4bd2d9245d822b6ff761f9307e931f62
Chigo-OJ <chigozirimosuebi@gmail.com>
2023-03-17 18:10:49 -0700

    alphabet backwards

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - start of code
+ *
+ * Return: end of code
+ */
+
+int main(void)
+{
+	char i;
+
+	for (i = 'z'; i >= 'a'; i--)
+		putchar(i);
+	putchar('\n');
+	return (0);
+}
