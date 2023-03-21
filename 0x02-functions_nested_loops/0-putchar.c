89c3db26d04191daa59bfd789ce59e30505b2a4c
Chigo-OJ <chigozirimosuebi@gmail.com>
2023-03-21 15:16:00 -0700

    printing _putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * main - start of code block
+ * _putchar - prints a char
+ * Return: code ends and returns to caller
+ */
+int main(void)
+{
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
+	return (0);
+}
