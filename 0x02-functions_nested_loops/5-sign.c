f10f98569b57cd461b5230eae1b08647406bc0a5
Chigo-OJ <chigozirimosuebi@gmail.com>
2023-03-21 17:42:32 -0700

    signs of positive negative or equal n

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,33 @@
+#include "main.h"
+
+/**
+ * print_sign - prints signs of a number
+ * @n: parameter of the function
+ * Return: breaks out of code and returns to the caller 1, 0, -1
+ */
+
+
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		_putchar('+');
+		_putchar(',');
+		_putchar(' ');
+		return (1);
+	}
+	else if (n == 0)
+	{
+		_putchar('0');
+		_putchar(',');
+		_putchar(' ');
+		return (0);
+	}
+	else
+	{
+		_putchar('-');
+		_putchar(',');
+		_putchar(' ');
+		return (-1);
+	}
+}
