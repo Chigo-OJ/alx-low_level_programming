7fca7fed2fcb67ff90d6a45538dfaf263bb7f90b
Chigo-OJ <chigozirimosuebi@gmail.com>
2023-03-22 16:48:34 -0700

    sum of two numbers

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * add - adds two int
+ * @a: parameter 1
+ * @b: parameter 2
+ * Return: returns the sum of int a and b
+ */
+
+int add(int a, int b)
+{
+	return (a + b);
+}
