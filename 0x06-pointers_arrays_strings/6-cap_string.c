702ceb9170e96e561faefc0b506a17d3dec3113a
Chigo-OJ <chigozirimosuebi@gmail.com>
2023-03-30 17:07:56 -0700

    capitalize  strings

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -8,28 +8,28 @@
 
 char *cap_string(char *s)
 {
-	int string_count;
-/*scan through string*/
-	string_count = 0;
-	while (s[string_count] != '\0')
+	int str_count;
+
+	str_count = 0;
+	while (s[str_count] != '\0')
 /*if next charaxcter after count is char, capitalize it*/
 	{
 		if (s[0] >= 97 && s[0] <= 122)
 		{
 			s[0] = s[0] - 32;
 		}
-		if (s[string_count] == ' ' || s[string_count] == '\t' || s[string_count] == '\n'
-		|| s[string_count] == ',' || s[string_count] == ';' || s[string_count] == '.'
-		|| s[string_count] == '.' || s[string_count] == '!' || s[string_count] == '?'
-		|| s[string_count] == '"' || s[string_count] == '(' || s[string_count] == ')'
-		|| s[string_count] == '{' || s[string_count] == '}')
+		if (s[str_count] == ' ' || s[str_count] == '\t' || s[str_count] == '\n'
+		|| s[str_count] == ',' || s[str_count] == ';' || s[str_count] == '.'
+		|| s[str_count] == '.' || s[str_count] == '!' || s[str_count] == '?'
+		|| s[str_count] == '"' || s[str_count] == '(' || s[str_count] == ')'
+		|| s[str_count] == '{' || s[str_count] == '}')
 		{
-			if (s[string_count + 1] >= 97 && s[string_count + 1] <= 122)
+			if (s[str_count + 1] >= 97 && s[str_count + 1] <= 122)
 			{
-				s[string_count + 1] = s[string_count + 1] - 32;
+				s[str_count + 1] = s[str_count + 1] - 32;
 			}
 		}
-		string_count++;
+		str_count++;
 	}
 	return (s);
 }
