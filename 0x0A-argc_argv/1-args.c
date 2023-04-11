56d61561d28e917031b3baaf21f63b5a5fa4563b
Chigo-OJ <chigozirimosuebi@gmail.com>
2023-04-11 13:27:21 -0700

    printing the number of arguement passed to main

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - start of code execution
+ * @argc: arguement count to main
+ * @argv: arguement vector/ string to main
+ * Return: returns 0
+ */
+
+int main(int argc, char *argv[])
+{
+	if (argv[0])
+		printf("%d\n", argc);
+	return (0);
+}
