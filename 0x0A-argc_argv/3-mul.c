99531a54989efa0a211dc513fbf27c9f45e5cb69
Chigo-OJ <chigozirimosuebi@gmail.com>
2023-04-11 15:58:22 -0700

    multiplying two arguements

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -5,19 +5,25 @@
  * main - start of code that mutiplies two arguements
  * @argc: arguement count
  * @argv: arguement vector/ arguement string taken by main
- * Return: return 0
+ * Return: returns 0 or 1
  */
 
 int main(int argc, char *argv[])
 {
 	int ans;
-	int mul1 = atoi(argv[1]);
-	int mul2 = atoi(argv[2]);
+	int mul1;
+	int mul2;
 
-	ans = mul1 * mul2;
-
-		if (argc > 1)
+	if (argc != 3)
+	{
+		printf("Error\n");
+		return (1);
+	}
+	else
 		{
+			mul1 = atoi(argv[1]);
+			mul2 = atoi(argv[2]);
+			ans = mul1 * mul2;
 			printf("%d\n", ans);
 		}
 		return (0);
