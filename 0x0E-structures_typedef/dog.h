5cdf31348d619dd58b69b7733923bc7826182aa5
Chigo-OJ <chigozirimosuebi@gmail.com>
2023-04-18 15:20:47 -0700

    defining a type -struct dog

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,16 @@
+#ifndef _DOG_H_
+#define _DOG_H_
+
+/**
+ * struct dog - structure for a dog
+ * @name: Dog's name
+ * @age: Dog's age
+ * @owner: Dog's owner
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+#endif
