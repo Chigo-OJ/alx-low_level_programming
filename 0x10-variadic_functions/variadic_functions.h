2452bbcc4096c8a20a3c131048c34098301d9b13
Chigo-OJ <chigozirimosuebi@gmail.com>
2023-04-20 15:03:49 -0700

    sum of all parameters

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -1,5 +1,5 @@
-#ifndef VARIDIC_FUNCTIONS_H
-#define VARIDIC_FUNCTIONS_H
+#ifndef VARIADIC_FUNCTIONS_H
+#define VARIADIC_FUNCTIONS_H
 
 int _putchar(char c);
 int sum_them_all(const unsigned int n, ...);
